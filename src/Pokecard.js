4670c91da7f2c1266e9b9100d4c844154717929c
Mark Standen <mark@markstanden.uk>
2020-10-16 20:15:35 +0100

    pokecard base

---
+++ src/Pokecard.js
@@ -0,0 +1,9 @@
+import React, { component } from "react";
+
+class Pokecard extends Component {
+  render() {
+    return <h1> Pokecard </h1>;
+  }
+}
+
+export default Pokecard;
--- src/data.js
+++ src/data.js
@@ -3,3 +3,4 @@ export const pokeDB = [
   {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
   {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112}
 ]
+  
